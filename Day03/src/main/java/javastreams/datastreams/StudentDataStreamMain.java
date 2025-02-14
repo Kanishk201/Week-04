9c6a99d4886d658827148ff5bb12776a895509a9
Kanishk201 <kanisyadav88780@gmail.com>
2025-02-14 16:47:15 +0530

    [Kanishk] Add. Solution of Data Streams - Store and Retrieve Primitive Data

---
+++ Day03/src/main/java/javastreams/datastreams/StudentDataStreamMain.java
@@ -0,0 +1,9 @@
+package javastreams.datastreams;
+
+public class StudentDataStreamMain {
+    public static void main(String[] args) {
+        String fileName = "students";
+
+        StudentDataStream.studentDataStream(fileName);
+    }
+}
