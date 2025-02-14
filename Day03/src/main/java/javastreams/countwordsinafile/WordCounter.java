a709b8d4529e0e7c2cd5626dd8d427e20f1749de
Kanishk201 <kanisyadav88780@gmail.com>
2025-02-14 16:51:11 +0530

    [Kanishk] Add. Solution of counting words in a file

---
+++ Day03/src/main/java/javastreams/countwordsinafile/WordCounter.java
@@ -0,0 +1,32 @@
+package javastreams.countwordsinafile;
+
+import java.io.*;
+import java.util.*;
+
+public class WordCounter {
+    public static void wordCounter(String filepath) {
+        String filePath = "input";
+        Map<String, Integer> wordCount = new HashMap<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] words = line.toLowerCase().replaceAll("[^a-zA-Z ]", "").split("\\s+");
+                for (String word : words) {
+                    if (!word.isEmpty()) {
+                        wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading file: " + e.getMessage());
+        }
+
+
+        wordCount.entrySet().stream()
+                .sorted((a, b) -> b.getValue().compareTo(a.getValue()))
+                .limit(5)
+                .forEach(entry -> System.out.println(entry.getKey() + ": " + entry.getValue()));
+    }
+}
+
---
+++ Day03/src/main/java/javastreams/countwordsinafile/WordCounterMain.java
@@ -0,0 +1,10 @@
+package javastreams.countwordsinafile;
+
+public class WordCounterMain {
+    public static void main(String[] args) {
+
+        String filePath = "input";
+
+        WordCounter.wordCounter(filePath);
+    }
+}
