3a5fb685135bee7c96d399759bc8088e18f72fee
Kanishk201 <kanisyadav88780@gmail.com>
2025-02-15 23:45:37 +0530

    [Kanishk] Add. solution of Testing Date Formatter

---
+++ Day05/src/main/java/junitproblems/advancedjunitproblems/dateformattertesting/DateFormatter.java
@@ -0,0 +1,17 @@
+package junitproblems.advancedjunitproblems.dateformattertesting;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+public class DateFormatter {
+    public static String formatDate(String inputDate) {
+        try {
+            LocalDate date = LocalDate.parse(inputDate, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+            return date.format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+        } catch (DateTimeParseException e) {
+            return "Invalid date format";
+        }
+    }
+}
+
