61900bca23e1fdd1e171499520bc100d6c985fea
Kanishk201 <kanisyadav88780@gmail.com>
2025-02-15 23:38:30 +0530

    [Kanishk] Add. Solution of Testing List Operations

---
+++ Day05/src/main/java/junitproblems/basicjunitproblems/listoperations/ListManager.java
@@ -0,0 +1,21 @@
+package junitproblems.basicjunitproblems.listoperations;
+
+import java.util.List;
+
+public class ListManager {
+    public void addElement(List<Integer> list, int element) {
+        if (list != null) {
+            list.add(element);
+        }
+    }
+
+    public void removeElement(List<Integer> list, int element) {
+        if (list != null) {
+            list.remove(Integer.valueOf(element)); // Removes first occurrence of the element
+        }
+    }
+
+    public int getSize(List<Integer> list) {
+        return (list != null) ? list.size() : 0;
+    }
+}
