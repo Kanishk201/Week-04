6554a9f1986ce781cce6b54f736a696c39732cb9
Kanishk201 <kanisyadav88780@gmail.com>
2025-02-15 23:44:59 +0530

    [Kanishk] Add. Solution of Testing Temperature Converter

---
+++ Day05/src/main/java/junitproblems/advancedjunitproblems/temperatureconvertertesting/TemperatureConverter.java
@@ -0,0 +1,12 @@
+package junitproblems.advancedjunitproblems.temperatureconvertertesting;
+
+public class TemperatureConverter {
+    public static double celsiusToFahrenheit(double celsius) {
+        return (celsius * 9 / 5) + 32;
+    }
+
+    public static double fahrenheitToCelsius(double fahrenheit) {
+        return (fahrenheit - 32) * 5 / 9;
+    }
+}
+
