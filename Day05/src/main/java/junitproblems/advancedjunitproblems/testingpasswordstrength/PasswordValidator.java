028069a33715af1ebf17ce1ac1af048ec60ec25f
Kanishk201 <kanisyadav88780@gmail.com>
2025-02-15 23:44:19 +0530

    [Kanishk] Add. Solution of Testing Password Strength Validator

---
+++ Day05/src/main/java/junitproblems/advancedjunitproblems/testingpasswordstrength/PasswordValidator.java
@@ -0,0 +1,11 @@
+package junitproblems.advancedjunitproblems.testingpasswordstrength;
+
+public class PasswordValidator {
+    public static boolean isValid(String password) {
+        if (password == null) return false;
+        return password.length() >= 8 &&
+                password.matches(".*[A-Z].*") &&
+                password.matches(".*\\d.*");
+    }
+}
+
